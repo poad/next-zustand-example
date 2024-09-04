a4bb3f986f95ff7282b40362f519da50cc94389a
Kenji Saito <ken-yo@mbr.nifty.com>
2024-09-04 23:00:38 +0900

    Bumps node modules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,19 @@
+// @ts-check
+
+import eslint from '@eslint/js';
 import nextPlugin from '@next/eslint-plugin-next';
 import reactPlugin from 'eslint-plugin-react';
 import hooksPlugin from 'eslint-plugin-react-hooks';
-import typescriptParser from '@typescript-eslint/parser';
+import stylistic from '@stylistic/eslint-plugin';
+import stylisticTs from '@stylistic/eslint-plugin-ts';
+import stylisticJsx from '@stylistic/eslint-plugin-jsx';
+import tseslint from 'typescript-eslint';
 
-export default [
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
   {
-    languageOptions: {
-      parser: typescriptParser,
-    },
     files: ['**/*.ts', '**/*.tsx'],
     ignores: [
       '**/*.d.ts',
@@ -16,11 +22,15 @@ export default [
       'src/next-env.d.ts',
       'src/stories',
       'node_modules/**/*',
+      './.next/*',
     ],
     plugins: {
       react: reactPlugin,
       'react-hooks': hooksPlugin,
       '@next/next': nextPlugin,
+      '@stylistic': stylistic,
+      '@stylistic/ts': stylisticTs,
+      '@stylistic/jsx': stylisticJsx,
     },
     rules: {
       ...reactPlugin.configs['jsx-runtime'].rules,
@@ -30,9 +40,13 @@ export default [
       '@next/next/no-duplicate-head': 'off',
       '@next/next/no-img-element': 'error',
       '@next/next/no-page-custom-font': 'off',
+      '@stylistic/semi': 'error',
+      '@stylistic/ts/indent': ['error', 2],
+      '@stylistic/jsx/jsx-indent': ['error', 2],
+      'comma-dangle': ['error', 'always-multiline'],
+      'arrow-parens': ['error', 'always'],
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
     },
   },
-  {
-    ignores: ['./.next/*'],
-  },
-];
+);
